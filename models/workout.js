814e8e7821827a51e73527831ea1605a7115b137
nqg <nidaghuman@gmail.com>
2021-04-20 22:58:45 -0400

    fixed error in range function

--- models/workout.js
+++ models/workout.js
@@ -37,7 +37,6 @@ const workoutSchema = new Schema({
 });
 
 
-
 const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const mongoose = require("mongoose");
-const express = require("express");
 const Workout = require("../models/workout.js");
 
+//post route to create new workout
 router.post("/workouts", ({body}, res) => {
 Workout.create({})
 .then(dbWorkout => {
+    console.log(`post: ${dbWorkout}`)
     res.json(dbWorkout);
 })
 .catch(err => {
@@ -13,10 +13,18 @@ Workout.create({})
 });
 });
 
-
+//get route to find all workouts in db
 router.get("/workouts", (req, res) => {
- Workout.find({})
+Workout.find({})
+//Workout.aggregate ([
+   // {
+     //   $addFields: {
+     //       totalDuration: { $sum: "$duration"}
+     //   }
+   // }
+//])
  .then(dbWorkout => {
+     console.log(`get: ${dbWorkout}`);
      res.json(dbWorkout);
  })
  .catch (err => {
@@ -24,19 +32,7 @@ router.get("/workouts", (req, res) => {
  });
 });
 
-
-router.get("/workouts/:id", (req,res) => {
-    const id = req.params.id;
-    Workout.find({_id:id})
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    })
-})
-
-
+//put route to add to a workout
 router.put("/workouts/:id", ({params, body},res) => {
 
     Workout.findOneAndUpdate(
@@ -44,6 +40,7 @@ router.put("/workouts/:id", ({params, body},res) => {
         {$push: {exercises: body}}, 
         {new:true})
     .then(dbWorkout => {
+        console.log(`put: ${dbWorkout}`)
         res.json(dbWorkout);
     })
     .catch(err => {
@@ -51,11 +48,12 @@ router.put("/workouts/:id", ({params, body},res) => {
     });
 })
 
-
+//get route to retrieve last 7 workouts for stats page
 router.get("/workouts/range", (req,res) => {
     Workout.find({})
     .limit(7)
     .then(dbWorkout => {
+        console.log(`range: ${dbWorkout}`)
         res.json(dbWorkout);
     })
     .catch(err => {
